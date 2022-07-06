ff99ae8da63d7dcf43f3f735ebd134106e3dc08c
Jeffrey Han <39203126+elludraon@users.noreply.github.com>
2022-07-06 14:28:51 -0700

    fix(tg95): add missing build directory (#8)
    Signed-off-by: Jeffrey Han <39203126+elludraon@users.noreply.github.com>

---
+++ src/tg95/build/precache-manifest.8d3258ee41115abcf23d89b4dbe9fada.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9545dbad9800c99d3d5a2c8e99facd44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "657b77d188b9e538c9ba",
+    "url": "./static/js/2.4404512c.chunk.js"
+  },
+  {
+    "revision": "f01b6ee26155721017cc5fc8eb2b8875",
+    "url": "./static/js/2.4404512c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99da7414e8eb79548745",
+    "url": "./static/js/3.49f752ca.chunk.js"
+  },
+  {
+    "revision": "a7be164640a98ad548d77ef52146abc6",
+    "url": "./static/js/3.49f752ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1143476bb06d70af355d",
+    "url": "./static/js/4.fa1fb945.chunk.js"
+  },
+  {
+    "revision": "c38aca750eeaa6b8f4af",
+    "url": "./static/js/main.bf21edd6.chunk.js"
+  },
+  {
+    "revision": "a790fba2ca506410e191",
+    "url": "./static/js/runtime-main.b1dfee7a.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "./static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "./static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
